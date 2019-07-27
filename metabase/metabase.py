1850c74103729cbb596314ac4dbf754f94baca50
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-07-27 08:56:15 +0200

    Run 3to2 binary to convert code from python 3 to 2 - Error still present though (https://pypi.org/project/3to2/)

--- metabase/metabase.py
+++ metabase/metabase.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import os
 import time
 import requests
@@ -5,22 +6,29 @@ import requests
 
 class Metabase(object):
 
-    def __init__(self, *args, endpoint=None, email=None,
-                 password=None, session=None, **kwargs):
-        self.endpoint = endpoint or os.getenv('METABASE_ENDPOINT') + '/api'
-        self.email = email or os.getenv('METABASE_AUTH_EMAIL')
-        self.password = password or os.getenv('METABASE_AUTH_PASSWORD')
-        self.session = session or os.getenv('METABASE_SESSION')
-        self.auth_callback = kwargs.pop('auth_callback', None)
+    def __init__(self, *args, **kwargs):
+        if 'session' in kwargs: session = kwargs['session']; del kwargs['session']
+        else: session = None
+        if 'password' in kwargs: password = kwargs['password']; del kwargs['password']
+        else: password = None
+        if 'email' in kwargs: email = kwargs['email']; del kwargs['email']
+        else: email = None
+        if 'endpoint' in kwargs: endpoint = kwargs['endpoint']; del kwargs['endpoint']
+        else: endpoint = None
+        self.endpoint = endpoint or os.getenv(u'METABASE_ENDPOINT') + u'/api'
+        self.email = email or os.getenv(u'METABASE_AUTH_EMAIL')
+        self.password = password or os.getenv(u'METABASE_AUTH_PASSWORD')
+        self.session = session or os.getenv(u'METABASE_SESSION')
+        self.auth_callback = kwargs.pop(u'auth_callback', None)
 
         if self.session is None:
             self.auth()
 
     def session_header(self):
-        return {'X-Metabase-Session': self.session}
+        return {u'X-Metabase-Session': self.session}
 
     def get_session_headers(self, *args, **kwargs):
-        res = requests.get(self.endpoint + '/user/current',
+        res = requests.get(self.endpoint + u'/user/current',
                            headers=self.session_header())
         if res.status_code == 401:
             self.auth()
@@ -39,43 +47,51 @@ class Metabase(object):
             return False, None
 
     def _get_session_headers(self, kwargs):
-        if not kwargs.pop('check_session', True):
+        if not kwargs.pop(u'check_session', True):
             return self.get_session_header(**kwargs)
         return headers or {}
 
-    def get(self, *args, headers=None, **kwargs):
+    def get(self, *args, **kwargs):
+        if 'headers' in kwargs: headers = kwargs['headers']; del kwargs['headers']
+        else: headers = None
         headers = self.get_session_headers(headers, kwargs)
         r = requests.get(self.endpoint + args[0], headers=headers, **kwargs)
         return self.fetch_body(r)
 
-    def post(self, *args, headers=None, **kwargs):
+    def post(self, *args, **kwargs):
+        if 'headers' in kwargs: headers = kwargs['headers']; del kwargs['headers']
+        else: headers = None
         headers = self.get_session_headers(headers, kwargs)
         r = requests.post(self.endpoint + args[0], headers=headers, **kwargs)
         return self.fetch_body(r)
 
-    def put(self, *args, headers=None, **kwargs):
+    def put(self, *args, **kwargs):
+        if 'headers' in kwargs: headers = kwargs['headers']; del kwargs['headers']
+        else: headers = None
         headers = self.get_session_headers(headers, kwargs)
         r = requests.put(self.endpoint + args[0], headers=headers, **kwargs)
         return self.fetch_header(r)
 
-    def delete(self, *args, headers=None, **kwargs):
+    def delete(self, *args, **kwargs):
+        if 'headers' in kwargs: headers = kwargs['headers']; del kwargs['headers']
+        else: headers = None
         headers = self.get_session_headers(headers, kwargs)
         r = requests.put(self.endpoint + args[0], headers=headers, **kwargs)
         return self.fetch_header(r)
 
     def auth(self, **kwargs):
         payload = {
-            'username': self.email,
-            'password': self.password
+            u'username': self.email,
+            u'password': self.password
         }
 
-        res = requests.post(self.endpoint + '/session', json=payload)
+        res = requests.post(self.endpoint + u'/session', json=payload)
 
         if res.status_code == 200:
             data = res.json()
-            self.session = data['id']
+            self.session = data[u'id']
         else:
             raise Exception(res)
 
-        if hasattr(self, 'auth_callback') and callable(self.auth_callback):
+        if hasattr(self, u'auth_callback') and callable(self.auth_callback):
             self.auth_callback(self)
